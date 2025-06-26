9d1e4abc7c10ed99c993a2e59127021af0967a43
RobertoPSF <88672689+RobertoPSF@users.noreply.github.com>
2025-06-26 16:41:01 -0300

    unifying user_invitations

--- nodes/AppStore/operations/user_invitations/getById.ts
+++ nodes/AppStore/operations/user_invitations/getById.ts
@@ -1,10 +1,15 @@
-import { getUserInvitationById } from "../../requests/users_invitations/read_user_invit_inf";
+import { appStoreGeneralRequest } from "../../requests/general_request";
+import { ROUTE_USER_INVITATION_BY_ID } from "../../requests/api_routes";
 
-
-export async function node_get_user_invitation(context: any, jwtToken: string){
-    const userId = context.getNodeParameter('id', 0) as string;
+export async function node_get_user_invitation(context: any, jwtToken: string) {
+    const invitationId = context.getNodeParameter('id', 0) as string;
     try {
-        const response = await getUserInvitationById(context.helpers, jwtToken, userId);
+        const response = await appStoreGeneralRequest({
+            method: 'GET',
+            endpoint: ROUTE_USER_INVITATION_BY_ID(invitationId),
+            jwtToken,
+            helpers: context.helpers,
+        });
         if (response.data) {
             return response.data;
         } else {
--- nodes/AppStore/requests/api_routes.ts
+++ nodes/AppStore/requests/api_routes.ts
@@ -3,3 +3,4 @@ export const ROUTE_USER_BY_ID = (userId: string) => `/v1/users/${userId}`;
 export const ROUTE_USER_VISIBLE_APPS = (userId: string) => `/v1/users/${userId}/visibleApps`;
 export const ROUTE_USER_VISIBLE_APPS_REL = (userId: string) => `/v1/users/${userId}/relationships/visibleApps`;
 export const ROUTE_USER_INVITATIONS = '/v1/userInvitations';
+export const ROUTE_USER_INVITATION_BY_ID = (invitationId: string) => `/v1/userInvitations/${invitationId}`;
