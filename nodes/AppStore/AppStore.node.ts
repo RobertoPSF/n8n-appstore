bee285e87fa1d0f69a22403b8c1d4aa32905bd5c
steylor <steylornb@gmail.com>
2025-06-26 13:06:28 -0300

    fix

--- nodes/AppStore/AppStore.node.ts
+++ nodes/AppStore/AppStore.node.ts
@@ -15,9 +15,9 @@ import {
 import { node_modify_user } from './operations/user/modify';
 import { node_list_user } from './operations/user/list';
 import { node_get_user } from './operations/user/getById';
+import { node_list_invitated_users } from './operations/user_invitations/list';
 import { MODIFY_USER_ALL_APPS_VISIBLE_SWITCH, MODIFY_USER_PROVISIONING_ALLOWED_SWITCH, MODIFY_USER_ROLES_FIELD, MODIFY_USER_VISIBLE_APP_IDS } from './fields/users/modify_user_fields';
 import { MODIFY_USER_USER_ID_FIELD } from './fields/users/user_get_by_id_fields';
-import { node_list_user_permissions } from './operations/user_permissions/list';
 
 interface IAppStoreApiCredentials extends ICredentialDataDecryptedObject {
 	issuerId: string;
@@ -66,9 +66,9 @@ export class AppStore implements INodeType {
 						description: 'Modify a user account',
 					},
 					{
-						name: 'List Users Permissions',
-						value: 'listUsersPermissions',
-						description: 'get a list of users permissions'
+						name: 'List Invitated Users',
+						value: 'listInvitatedUsers',
+						description: 'get a list of invitated users'
 					}
 				],
 				default: '',
@@ -93,7 +93,7 @@ export class AppStore implements INodeType {
 		if (operation === 'listUsers') returnData = await node_list_user(this, jwtToken);
 		if (operation === 'getUserById') returnData.push(await node_get_user(this, jwtToken));
 		if (operation === 'modifyUser') returnData.push(await node_modify_user(this, jwtToken));
-		if (operation === 'listUsersPermissions') returnData.push(await node_list_user_permissions(this, jwtToken));
+		if (operation === 'listInvitatedUsers') returnData.push(await node_list_invitated_users(this, jwtToken));
 		return [this.helpers.returnJsonArray(returnData)];
 	}
 }
